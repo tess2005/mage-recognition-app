cd7fd5da284b090e6349d8ee91eb67e2db5efc93
Tess Thomas <tessthomas360@gmail.com>
2026-05-19 21:59:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import torch
 from torchvision import models, transforms
+import requests
 
 # 1. Load the "Brain"
 @st.cache_resource
@@ -12,7 +13,15 @@ def load_model():
 
 model = load_model()
 
-# 2. Setup Image processing
+# 2. Get the labels
+def get_labels():
+    url = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
+    response = requests.get(url)
+    return response.text.splitlines()
+
+labels = get_labels()
+
+# 3. Setup Image processing
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -31,7 +40,10 @@ if uploaded_file is not None:
     input_tensor = preprocess(image).unsqueeze(0)
     with torch.no_grad():
         output = model(input_tensor)
-        _, predicted = torch.max(output, 1)
+        _, predicted_idx = torch.max(output, 1)
         
+    # Get the name of the object
+    predicted_label = labels[predicted_idx.item()]
+    
     st.write(f"### AI Analysis Result:")
-    st.write(f"The model detected index: {predicted.item()} (I will add labels next!)")
+    st.write(f"The AI thinks this is: **{predicted_label}**")
